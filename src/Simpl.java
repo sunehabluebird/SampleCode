eac69c8871a2cce40c29d4bb743c13640bb4cbc9
Suneha <gatiganti.sune@gmail.com>
2022-03-15 07:29:30 +0530

    added file

---
+++ src/Simpl.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Simpl
+{
+
+	
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chandu\\Documents\\SeleniumDriverChrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/login/");
+	}
+
+}
